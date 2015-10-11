ec66ed5fb42a68c266160fe62f708f1f4ab20c35
Luke Church <luke@church.name>
2015-10-11 22:53:54 +0100

    Source Uploader to handle multiple files

--- sintr_common/bin/uploadSource.dart
+++ sintr_common/bin/uploadSource.dart
@@ -11,13 +11,15 @@ import 'package:sintr_common/auth.dart';
 import "package:sintr_common/configuration.dart" as config;
 import "package:sintr_common/gae_utils.dart" as gae_utils;
 import 'package:sintr_common/logging_utils.dart';
+import 'package:path/path.dart' as path_utils;
 
 main(List<String> args) async {
   setupLogging();
 
   if (args.length != 4) {
-    print ("Pack and upload source for sintr_worker");
-    print("Usage: dart uploadSource.dart project_name bucket_name object_name path");
+    print("Pack and upload source for sintr_worker");
+    print(
+        "Usage: dart uploadSource.dart project_name bucket_name object_name path");
     print(args);
     io.exit(1);
   }
@@ -27,21 +29,43 @@ main(List<String> args) async {
   String objectName = args[2];
   String path = args[3];
 
+  if (path.endsWith('/')) {
+    print ("Please use unterimanted paths");
+    io.exit(1);
+  }
+
+  var dartFiles = new io.Directory(path)
+      .listSync(recursive: true)
+      .where((fse) => fse.path.endsWith(".dart"));
+
+  // Compute paths relative to the source
+  // -> Sub path, strip '/' from the start
+  var relativePaths = dartFiles
+      .map((fse) => fse.path.split(path)[1].substring(1))
+      .where((p) => !(p.contains('packages') || p.contains('.pub')))
+      .toList();
+
   config.configuration = new config.Configuration(projectName,
-      cryptoTokensLocation: "${config.userHomePath}/Communications/CryptoTokens");
+      cryptoTokensLocation:
+          "${config.userHomePath}/Communications/CryptoTokens");
 
   var client = await getAuthedClient();
 
   ss.fork(() async {
     storage.registerStorageService(new storage.Storage(client, projectName));
 
-    String source = new io.File(path).readAsStringSync();
-    var json = JSON.encode({"worker_isolate.dart" : source});
+    var sourceMap = {};
+
+    for (String relativePath in relativePaths) {
+      sourceMap.putIfAbsent(
+          relativePath, () => new io.File("$path/$relativePath").readAsStringSync());
+    }
+    var json = JSON.encode(sourceMap);
 
     List<int> bytes = UTF8.encode(json);
 
     await gae_utils.CloudStorage.writeFileBytes(bucketName, objectName, bytes);
 
-    info ("Upload done");
+    info("Upload done");
   });
 }

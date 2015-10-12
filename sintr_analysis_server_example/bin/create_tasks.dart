8a3f180817b2573699e7906b607abefad314173d
Luke Church <luke@church.name>
2015-10-12 01:02:28 +0100

    Cleanup

--- sintr_analysis_server_example/bin/create_tasks.dart
+++ sintr_analysis_server_example/bin/create_tasks.dart
@@ -2,19 +2,23 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io' as io;
 import 'package:gcloud/storage.dart' as storage;
 import 'package:sintr_common/auth.dart';
 import "package:sintr_common/configuration.dart" as config;
 import 'package:sintr_common/logging_utils.dart';
 import 'package:sintr_common/task_utils.dart' as task_utils;
-import 'package:sintr_common/tasks.dart' as tasks;
 
 main(List<String> args) async {
   setupLogging();
 
+  if (args.length != 0) {
+    print("Create tasks for workers");
+    io.exit(1);
+  }
+  
   String projectId = "liftoff-dev";
   String inputDataBucket = "liftoff-dev-datasources";
-  int bucketSize = 100;
 
   config.configuration = new config.Configuration(projectId,
       cryptoTokensLocation:
--- sintr_analysis_server_example/bin/reset.dart
+++ sintr_analysis_server_example/bin/reset.dart
@@ -2,25 +2,29 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io' as io;
+
 import 'package:gcloud/db.dart' as db;
 import 'package:gcloud/service_scope.dart' as ss;
 import 'package:gcloud/src/datastore_impl.dart' as datastore_impl;
 import 'package:gcloud/storage.dart' as storage;
 import 'package:sintr_common/auth.dart';
-import 'package:sintr_common/auth.dart';
-import "package:sintr_common/configuration.dart" as config;
 import "package:sintr_common/configuration.dart" as config;
 import 'package:sintr_common/logging_utils.dart';
 import 'package:sintr_common/logging_utils.dart' as log;
 import 'package:sintr_common/tasks.dart' as tasks;
-import 'package:sintr_common/tasks.dart' as task;
 
 main(List<String> args) async {
   setupLogging();
 
+  if (args.length != 1 || args[0] != "--force") {
+    print("Clear results buckets and all control datastore elements");
+    print("Usage dart reset.dart --force");
+    io.exit(1);
+  }
+
   String projectId = "liftoff-dev";
   String resultsBucket = "liftoff-dev-results";
-  int bucketSize = 100;
 
   config.configuration = new config.Configuration(projectId,
       cryptoTokensLocation:
@@ -56,9 +60,3 @@ main(List<String> args) async {
   });
   print("Tasks deleted");
 }
-
-// await taskController.deleteAllTasks();
-// log.info("Tasks deleted");
-
-// tasks.TaskController taskController =
-//     new tasks.TaskController("example_task");

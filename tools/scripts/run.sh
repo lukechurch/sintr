26c3236b62c1b65716617d69f7b53ada2904d00f
Luke Church <luke@church.name>
2015-10-12 11:27:59 +0100

    Multizone worker scripts
    Refactor to functions, allow easy deployment to multi-zone deployments

---
+++ tools/scripts/run.sh
@@ -0,0 +1,24 @@
+echo "Resetting"
+dart sintr_analysis_server_example/bin/reset.dart --force
+
+echo "Deploying Sintr"
+./tools/scripts/deploy_image.sh
+
+echo "Deploying Client code"
+./tools/scripts/upload_sample_src.sh
+
+# The next part is slow and can be done in parrallel
+
+echo "Deploying cluster"
+./tools/deploy_cluster.sh &
+
+echo "Creating tasks" &
+dart sintr_analysis_server_example/bin/create_tasks.dart
+
+echo "Starting monitoring loop"
+dart sintr_analysis_server_example/bin/query.dart --loop
+
+
+# When we're here the job is done
+echo "Deleting cluster"
+./tools/delete_cluster.sh

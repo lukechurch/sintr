09f4555c9cc388b1379738d9e63c1d8314964bb8
danrubel <danrubel@google.com>
2015-11-05 18:12:12 -0500

    first cut of converting extraction methods to Mapper interface

--- sintr_analysis_server_example/lib/completion_metrics.dart
+++ sintr_analysis_server_example/lib/completion_metrics.dart
@@ -1,44 +1,18 @@
 library sintr_worker_lib.completion;
 
+import 'dart:async';
 import 'dart:math' as math;
 
+import 'package:sintr_worker_lib/query.dart';
 import 'package:sintr_worker_lib/session_info.dart';
 
-/// Initialize the completion extraction process
-final completionExtractionStart = (Map sessionInfo) {
-  _sdkVersion = sessionInfo[SDK_VERSION] ?? 'unknown';
-};
-
-/// Process a log entry and return a string representing the result
-/// or `null` if no result from the given log entry.
-final completionExtraction = (String logEntryText) {
-  if (logEntryText == null || logEntryText == "") return null;
-  if (!logEntryText.startsWith("~")) return null;
-
-  int index1 = logEntryText.indexOf(':', 1);
-  if (index1 == -1) return null;
-  int time = int.parse(logEntryText.substring(1, index1));
-
-  int index2 = logEntryText.indexOf(':', index1 + 1);
-  if (index2 == -1) return null;
-  String msgType = logEntryText.substring(index1 + 1, index2);
-
-  return _processLogMessage(time, msgType, logEntryText.substring(index2 + 1));
-};
-
-/// Reporting any partial completion information
-final completionExtractionFinished = () {
-  var results = [];
-  for (_Completion completion in _requestMap.values) {
-    results.add(_composeExtractionResult(completion, -1));
-  }
-  _requestMap.clear();
-  for (_Completion completion in _notificationMap.values) {
-    results.add(_composeExtractionResult(completion, -2));
-  }
-  _notificationMap.clear();
-  return results;
-};
+const AVE = 'ave';
+const COUNT = 'count';
+const INCOMPLETE = 'incomplete';
+const MAX = 'max';
+const MIN = 'min';
+const TOTAL = 'total';
+const VERSION = 'version';
 
 /// Process log line that was encoded by [_composeExtractionResult]
 /// and return a map of current results
@@ -108,124 +82,166 @@ final completionReductionMerge = (Map results1, Map results2) {
   return newResults;
 };
 
-final AVE = 'ave';
-final COUNT = 'count';
-final INCOMPLETE = 'incomplete';
-final MAX = 'max';
-final MIN = 'min';
 final OPEN_BRACE = '{'.codeUnitAt(0);
 final QUOTE = '"'.codeUnitAt(0);
-final TOTAL = 'total';
-final VERSION = 'version';
 
-/// The SDK version
-String _sdkVersion = 'unset';
+/// [CompletionMapper] processes session log messages and extracts
+/// metrics for each call to code completion
+class CompletionMapper extends Mapper {
 
-/// A mapping of completion notification ID to information abou the completion.
-/// Elements are added when a completion response is found
-/// and removed when the final notification is found.
-Map<String, _Completion> _notificationMap = <String, _Completion>{};
+  /// A mapping of completion notification ID to information abou the completion.
+  /// Elements are added when a completion response is found
+  /// and removed when the final notification is found.
+  Map<String, _Completion> _notificationMap = <String, _Completion>{};
 
-/// A mapping of request ID to information about the completion request.
-/// Elements are added when a request is found
-/// and removed when a matching reponse is found
-Map<String, _Completion> _requestMap = <String, _Completion>{};
+  /// A mapping of request ID to information about the completion request.
+  /// Elements are added when a request is found
+  /// and removed when a matching reponse is found
+  Map<String, _Completion> _requestMap = <String, _Completion>{};
 
-String _composeExtractionResult(_Completion completion, int completionTime) =>
-    '$_sdkVersion,$completionTime';
+  /// The SDK version
+  String _sdkVersion = 'unset';
 
-/// Search the given [logMessageText] for the given [key]
-/// and return the associated value.
-String _extractJsonValue(String logMessageText, String key) {
-  var prefix = '"$key"::';
-  int start = logMessageText.indexOf(prefix);
-  if (start == -1) throw 'expected $key in $logMessageText';
-  start += prefix.length;
-  int deliminator = logMessageText.codeUnitAt(start);
-  if (deliminator == QUOTE) {
-    // Return quoted string
-    ++start;
-    int end = logMessageText.indexOf('"', start);
-    if (end == -1) throw 'expected value for $key in $logMessageText';
-    return logMessageText.substring(start, end);
-  } else if (deliminator == OPEN_BRACE) {
-    // Return JSON map
-    // TODO (danrubel) handle nested and embedded braces
-    int end = logMessageText.indexOf('}');
-    if (end == -1) throw 'expected matching brace for $key in $logMessageText';
-    return logMessageText.substring(start, end + 1);
-  }
-  return null;
-}
-
-/// Process a log message and return a string representing the result
-/// or `null` if no result from the given log entry.
-String _processLogMessage(int time, String msgType, String logMessageText) {
-  if (msgType == 'Req') {
-    String method = _extractJsonValue(logMessageText, 'method');
-    return _processRequest(time, method, logMessageText);
-  } else if (msgType == 'Res') {
-    String requestId = _extractJsonValue(logMessageText, 'id');
-    return _processResponse(time, requestId, logMessageText);
-  } else if (msgType == 'Noti') {
-    String event = _extractJsonValue(logMessageText, 'event');
-    return _processNotification(time, event, logMessageText);
-  } else if (msgType == 'Read') {
-    return null;
-  } else if (msgType == 'Task') {
-    return null;
-  } else if (msgType == 'Log') {
-    return null;
-  } else if (msgType == 'Perf') {
-    return null;
-  } else if (msgType == 'Watch') {
-    return null;
-  }
-  //throw 'unknown msgType $msgType in $logMessageText';
-  return null;
-}
-
-/// Process a log entry representing a notification
-String _processNotification(int time, String event, String logMessageText) {
-  if (event == 'completion.results') {
-    if (!logMessageText.endsWith(',"isLast"::true}}')) return null;
-    var prefix = '{"event"::"completion.results","params"::{"id"::"';
-    if (!logMessageText.startsWith(prefix)) {
-      throw 'expected $prefix in $logMessageText';
+  /// Reporting any partial completion information
+  @override
+  List<String> cleanup() {
+    List<String> results = [];
+    for (_Completion completion in _requestMap.values) {
+      // TODO (danrubel) provide better message for incomplete requests
+      results.add(_composeExtractionResult(completion, -1));
     }
-    int start = prefix.length;
-    int end = logMessageText.indexOf('"', start);
-    if (end == -1) throw 'expected notification id in $logMessageText';
-    String notificationId = logMessageText.substring(start, end);
-    _Completion completion = _notificationMap.remove(notificationId);
-    if (completion == null) {
-      throw 'expected completion request for $logMessageText';
+    _requestMap.clear();
+    for (_Completion completion in _notificationMap.values) {
+      // TODO (danrubel) provide better message for missing notifications
+      results.add(_composeExtractionResult(completion, -2));
     }
-    return _composeExtractionResult(completion, time - completion.requestTime);
+    _notificationMap.clear();
+    return results;
   }
-  return null;
-}
 
-/// Process a log entry representing a request
-String _processRequest(int time, String method, String logMessageText) {
-  // Look for completion requests
-  if (method == 'completion.getSuggestions') {
-    String requestId = _extractJsonValue(logMessageText, 'id');
-    _requestMap[requestId] = new _Completion(time);
+  /// Initialize the completion extraction process
+  @override
+  Future init(Map<String, dynamic> sessionInfo) async {
+    _sdkVersion = sessionInfo[SDK_VERSION] ?? 'unknown';
   }
-  return null;
-}
 
-/// Process a log entry representing a response
-String _processResponse(int time, String requestId, String logMessageText) {
-  _Completion completion = _requestMap.remove(requestId);
-  if (completion != null) {
-    String result = _extractJsonValue(logMessageText, 'result');
-    String notificationId = _extractJsonValue(result, 'id');
-    _notificationMap[notificationId] = completion;
-    completion.responseTime = time;
+  /// Process a log entry and return a string representing the result
+  /// or `null` if no result from the given log entry.
+  @override
+  String map(String logEntryText) {
+    if (logEntryText == null || logEntryText == "") return null;
+    if (!logEntryText.startsWith("~")) return null;
+
+    int index1 = logEntryText.indexOf(':', 1);
+    if (index1 == -1) return null;
+    int time = int.parse(logEntryText.substring(1, index1));
+
+    int index2 = logEntryText.indexOf(':', index1 + 1);
+    if (index2 == -1) return null;
+    String msgType = logEntryText.substring(index1 + 1, index2);
+
+    return _processLogMessage(
+        time, msgType, logEntryText.substring(index2 + 1));
   }
-  return null;
+
+  String _composeExtractionResult(_Completion completion, int completionTime) =>
+      '$_sdkVersion,$completionTime';
+
+  /// Search the given [logMessageText] for the given [key]
+  /// and return the associated value.
+  String _extractJsonValue(String logMessageText, String key) {
+    var prefix = '"$key"::';
+    int start = logMessageText.indexOf(prefix);
+    if (start == -1) throw 'expected $key in $logMessageText';
+    start += prefix.length;
+    int deliminator = logMessageText.codeUnitAt(start);
+    if (deliminator == QUOTE) {
+      // Return quoted string
+      ++start;
+      int end = logMessageText.indexOf('"', start);
+      if (end == -1) throw 'expected value for $key in $logMessageText';
+      return logMessageText.substring(start, end);
+    } else if (deliminator == OPEN_BRACE) {
+      // Return JSON map
+      // TODO (danrubel) handle nested and embedded braces
+      int end = logMessageText.indexOf('}');
+      if (end ==
+          -1) throw 'expected matching brace for $key in $logMessageText';
+      return logMessageText.substring(start, end + 1);
+    }
+    return null;
+  }
+
+  /// Process a log message and return a string representing the result
+  /// or `null` if no result from the given log entry.
+  String _processLogMessage(int time, String msgType, String logMessageText) {
+    if (msgType == 'Req') {
+      String method = _extractJsonValue(logMessageText, 'method');
+      return _processRequest(time, method, logMessageText);
+    } else if (msgType == 'Res') {
+      String requestId = _extractJsonValue(logMessageText, 'id');
+      return _processResponse(time, requestId, logMessageText);
+    } else if (msgType == 'Noti') {
+      String event = _extractJsonValue(logMessageText, 'event');
+      return _processNotification(time, event, logMessageText);
+    } else if (msgType == 'Read') {
+      return null;
+    } else if (msgType == 'Task') {
+      return null;
+    } else if (msgType == 'Log') {
+      return null;
+    } else if (msgType == 'Perf') {
+      return null;
+    } else if (msgType == 'Watch') {
+      return null;
+    }
+    //throw 'unknown msgType $msgType in $logMessageText';
+    return null;
+  }
+
+  /// Process a log entry representing a notification
+  String _processNotification(int time, String event, String logMessageText) {
+    if (event == 'completion.results') {
+      if (!logMessageText.endsWith(',"isLast"::true}}')) return null;
+      var prefix = '{"event"::"completion.results","params"::{"id"::"';
+      if (!logMessageText.startsWith(prefix)) {
+        throw 'expected $prefix in $logMessageText';
+      }
+      int start = prefix.length;
+      int end = logMessageText.indexOf('"', start);
+      if (end == -1) throw 'expected notification id in $logMessageText';
+      String notificationId = logMessageText.substring(start, end);
+      _Completion completion = _notificationMap.remove(notificationId);
+      if (completion == null) {
+        throw 'expected completion request for $logMessageText';
+      }
+      return _composeExtractionResult(
+          completion, time - completion.requestTime);
+    }
+    return null;
+  }
+
+  /// Process a log entry representing a request
+  String _processRequest(int time, String method, String logMessageText) {
+    // Look for completion requests
+    if (method == 'completion.getSuggestions') {
+      String requestId = _extractJsonValue(logMessageText, 'id');
+      _requestMap[requestId] = new _Completion(time);
+    }
+    return null;
+  }
+
+  /// Process a log entry representing a response
+  String _processResponse(int time, String requestId, String logMessageText) {
+    _Completion completion = _requestMap.remove(requestId);
+    if (completion != null) {
+      String result = _extractJsonValue(logMessageText, 'result');
+      String notificationId = _extractJsonValue(result, 'id');
+      _notificationMap[notificationId] = completion;
+      completion.responseTime = time;
+    }
+    return null;
+  }
 }
 
 /// Information about a completion request / response / notification group
--- sintr_analysis_server_example/test_driver.dart
+++ sintr_analysis_server_example/test_driver.dart
@@ -8,97 +8,121 @@ import 'dart:io' as io;
 
 import 'package:sintr_worker_lib/completion_metrics.dart';
 import 'package:sintr_worker_lib/instrumentation_transformer.dart';
+import 'package:sintr_worker_lib/query.dart';
 import 'package:sintr_worker_lib/session_info.dart';
 
 main(List<String> args) async {
+  // Extract arguments
   if (args.length != 1) {
     print("Runs the analysis on a file locally");
     print("Usage: test_driver.dart logFile");
     io.exit(1);
   }
-
   String path = args[0];
-  var sessionInfo = await loadSessionInfo(path);
-  var f = new io.File(path);
-
-  completionExtractionStart(sessionInfo);
+  var file = new io.File(path);
   var extracted = <String>[];
 
-  // Process each line to extract information
-  var readFailureCount = 0;
-  var msgCount = 0;
-  var resultCount = 0;
-  var failureCount = 0;
-  await for (String logEntry in f
+  // Initialize query specific objects
+  TestMapper mapper = new TestMapper(new CompletionMapper(), path);
+  var reducer = completionReducer;
+  var reductionMerge = completionReductionMerge;
+
+  // Extraction
+  await mapper.init({});
+  await for (String logEntry in file
       .openRead()
       .transform(UTF8.decoder)
       .transform(new LineSplitter())
       .transform(new LogItemTransformer())
       .handleError((e, s) {
-    ++readFailureCount;
+    ++mapper.readFailureCount;
     print("Error reading line\n${trim300(e.toString())}\n$s");
   })) {
-    ++msgCount;
-    try {
-      var result = completionExtraction(logEntry);
-      if (result != null) {
-        print(result);
-        if (extracted != null) extracted.add(result);
-        ++resultCount;
-      }
-    } catch (e, s) {
-      ++failureCount;
-      print('$e\n$s');
+    var result = mapper.map(logEntry);
+    if (result != null) {
+      extracted.add(result);
     }
   }
-  print('Extraction summary:');
-  print('  $readFailureCount read failures');
-  print('  $msgCount messages processed');
-  print('  $resultCount extraction results');
-  print('  $failureCount extraction exceptions');
-
-  // Finish the extraction process
-  var finalResults = completionExtractionFinished();
-  if (finalResults is String) {
-    print(finalResults);
-  } else if (finalResults is List<String>) {
-    finalResults.forEach((ln) {
-      extracted.add(ln);
-      print(ln);
-    });
-  }
+  extracted.addAll(mapper.cleanup());
 
   // Reduce the information into two separate result maps
   var reduced1 = <String, dynamic>{};
   var reduced2 = <String, dynamic>{};
   int index = 0;
   while (index < extracted.length / 2) {
-    reduced1 = completionReducer(extracted[index], reduced1);
+    reduced1 = reducer(extracted[index], reduced1);
     ++index;
   }
   print(reduced1);
   while (index < extracted.length) {
-    reduced2 = completionReducer(extracted[index], reduced2);
+    reduced2 = reducer(extracted[index], reduced2);
     ++index;
   }
   print(reduced2);
 
   // Merge the result maps
-  var reduced = completionReductionMerge(reduced1, reduced2);
+  var reduced = reductionMerge(reduced1, reduced2);
   print(reduced);
 }
 
-Future<Map> loadSessionInfo(String path) async {
-  if (!path.endsWith('.json')) throw 'expected *.json';
-  var index = path.indexOf('-', path.lastIndexOf('/'));
-  var sessionId = path.substring(index + 1, path.length - 5);
-  if (sessionId.startsWith('PRI')) {
-    return {SESSION_ID: sessionId.substring(3)};
+/// [TestMapper] wraps another mapper with exception handling
+/// and performance monitoring for testing purposes.
+class TestMapper extends Mapper {
+  final Mapper mapper;
+  final String sessionFilePath;
+  int msgCount = 0;
+  int resultCount = 0;
+  int failureCount = 0;
+  int readFailureCount = 0;
+
+  TestMapper(this.mapper, this.sessionFilePath);
+
+  @override
+  List<String> cleanup() {
+    List<String> finalResults = mapper.cleanup();
+    resultCount += finalResults.length;
+    finalResults.forEach((ln) => print(ln));
+
+    print('Extraction summary:');
+    print('  $readFailureCount read failures');
+    print('  $msgCount messages processed');
+    print('  $resultCount extraction results');
+    print('  $failureCount extraction exceptions');
+    return finalResults;
   }
 
-  var priPath = '${path.substring(0, index)}-PRI$sessionId.json';
-  var f = new io.File(priPath);
-  Stream<List<int>> stream = f.openRead();
+  @override
+  Future init(Map<String, dynamic> sessionInfo) async {
+    if (!sessionFilePath.endsWith('.json')) throw 'expected *.json';
+    var index = sessionFilePath.indexOf('-', sessionFilePath.lastIndexOf('/'));
+    var name = sessionFilePath.substring(index + 1, sessionFilePath.length - 5);
+    Map sessionInfo;
+    if (name.startsWith('PRI')) {
+      // Provide simplified session info for a PRI file
+      return {SESSION_ID: name.substring(3)};
+    } else {
+      // Read the session info from the associated PRI file
+      var priPath = '${sessionFilePath.substring(0, index)}-PRI$name.json';
+      Stream<List<int>> stream = new io.File(priPath).openRead();
+      sessionInfo = await readSessionInfo(name, stream);
+    }
+    return mapper.init(sessionInfo);
+  }
 
-  return await readSessionInfo(sessionId, stream);
+  @override
+  String map(String logEntry) {
+    ++msgCount;
+    try {
+      var result = mapper.map(logEntry);
+      if (result != null) {
+        ++resultCount;
+        print(result);
+        return result;
+      }
+    } catch (e, s) {
+      ++failureCount;
+      print('$e\n$s');
+    }
+    return null;
+  }
 }

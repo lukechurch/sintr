0024ee09a763429bd8b741f1b410002221731e49
Luke Church <lukechurch@google.com>
2016-04-05 18:24:16 +0100

    Only report clusters for last version

--- analysis_server_example/local_stacktrace_cluster.dart
+++ analysis_server_example/local_stacktrace_cluster.dart
@@ -4,6 +4,7 @@
 
 import 'dart:convert';
 import 'dart:io' as io;
+import 'package:semver/semver.dart';
 
 import 'package:sintr_worker_lib/stacktrace_model.dart' as st;
 import 'package:nectar/clusterers.dart' as cluster;
@@ -21,12 +22,12 @@ main(List<String> args) async {
 
   Map<dynamic, String> fileMaps = {};
   var fses = new io.Directory(path).listSync();
+  Map<String, List<String>> versionsMap = {};
+  log("Index versions");
 
-  log("Loading ${fses.length} logs");
-
+  int i = 0;
   for (var fse in fses) {
-    // if (i++ > 1000) break;
-
+    log("${i++} / ${fses.length}");
     if (!fse.path.endsWith(".json")) continue;
     var f = new io.File(fse.path);
     var jsonMap = JSON.decode(f.readAsStringSync());
@@ -36,9 +37,50 @@ main(List<String> args) async {
     if (results.isEmpty) continue;
     for (var result in results) {
       var k = result[0];
+
+      try {
+        var version = new SemanticVersion.fromString(k);
+
+        versionsMap.putIfAbsent(k, () => []).add(fse.path);
+      } catch (e) {
+        print ("Invalid version: $k");
+      }
+      break;
+    }
+  }
+
+  log ("versionMap: $versionsMap");
+
+
+  var versionList = versionsMap.keys.toList()..sort((a, b) {
+    if (a == b) return 0;
+    if (new SemanticVersion.fromString(a) > new SemanticVersion.fromString(b)) return 1;
+    return -1;
+  });
+
+  var version = versionList.last;
+
+  log ("Processing version: $version");
+
+  log("Loading ${versionsMap[version].length} logs");
+
+  i = 0;
+  for (var path in versionsMap[version]) {
+    log("${i++} / ${versionsMap[version].length}");
+    // if (i++ > 1000) break;
+
+    if (!path.endsWith(".json")) continue;
+    var f = new io.File(path);
+    var jsonMap = JSON.decode(f.readAsStringSync());
+
+    List results = jsonMap["result"];
+    if (results == null) continue;
+    if (results.isEmpty) continue;
+    for (var result in results) {
+      var k = result[0];
       var v = result[1];
 
-      fileMaps[v] = fse.path;
+      fileMaps[v] = path;
 
       inMemoryKV.putIfAbsent(k, () => []);
       inMemoryKV[k].add(v);
@@ -61,8 +103,6 @@ main(List<String> args) async {
 
       for (String errReport in counts.keys) {
         try {
-          // print(errReport);
-
           String stString =
               errReport.split("message\"::\"")[1].split("\\n\"}}")[0];
 
@@ -110,15 +150,17 @@ main(List<String> args) async {
     }
 
     log("Unique stacks: ${dataItems.length}");
+    previouslyAdded.sort((a, b) => a.count.compareTo(b.count));
+    for (st.StackTrace st in previouslyAdded.reversed) {
+        log("Common stack: $st");
+        if (st.count <= 1) break;
+    }
 
     cluster.KMedoids clusterModel = new cluster.KMedoids(dataItems);
-
     log("Initial cost: ${clusterModel.cost}");
-
     while (clusterModel.step()) {
       log("Iteration cost: ${clusterModel.cost}");
     }
-
     log("$tracesVersion clustered in ${sw.elapsedMilliseconds} ms");
 
     for (var cluster in clusterModel.clusters) {
@@ -135,14 +177,11 @@ main(List<String> args) async {
 
 Map mergeResults(Map results1, Map results2) {
   Map newMerged = {};
-
   newMerged.addAll(results1);
-
   for (var k in results2.keys) {
     newMerged.putIfAbsent(k, () => 0);
     newMerged[k] += results2[k];
   }
-
   return newMerged;
 }
 
